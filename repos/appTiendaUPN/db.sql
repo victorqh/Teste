6a129250db45270aed0aa5f54c958ccc9304f03c
viczor <qhvictor@gmail.com>
2025-11-23 18:59:28 -0500

    +

--- repos/appTiendaUPN/db.sql
+++ repos/appTiendaUPN/db.sql
@@ -68,9 +68,7 @@ INSERT INTO categorias (nombre, descripcion) VALUES
 ('Gaming', 'Consolas, accesorios y periféricos gamer'),
 ('Accesorios', 'Accesorios tecnológicos variados');
 
-SELECT pg_get_serial_sequence('categorias', 'categoriaId');
-
-ALTER SEQUENCE public.categorias_categoriaId_seq RESTART WITH 1;
+ALTER SEQUENCE categorias_categoriaid_seq RESTART WITH 1;
 -- Productos de ejemplo
 
 -- LAPTOPS (Categoría 1)
