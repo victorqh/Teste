a761734b226555158e2dead42a96eb3175414f3a
victorqh <qhvictor@gmail.com>
2025-11-23 23:14:34 -0500

    +

---
+++ repos/Soluciones/appFinal_Backend/src/test/java/com/example/appfinal_backend/AppFinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appfinal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFinalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
